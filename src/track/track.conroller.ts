7d732ce122d889e2fc047589c3cf7b370080415d
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-12-08 17:38:26 +0200

    Track module has been created

---
+++ src/track/track.conroller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Delete, Get } from '@nestjs/common';
+
+@Controller('/tracks')
+export class TrackController {
+  create() {}
+
+  getAll() {}
+
+  getOne() {}
+
+  delete() {}
+}
